0de4f6246a9a231a28b7f31ba713f80817fe14c0
Liam <33222279+liam-mh@users.noreply.github.com>
2025-04-14 22:59:14 +0100

    feat/ comments

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+# -------------------------------------------------------------------
+# 	Project:      Robotics Task 2: Shopping Distributor
+#	  Author:       Liam Hammond C1022456 
+#                 Guy Nicklin C1009267
+#	  Created:      01/04/2025
+# -------------------------------------------------------------------
+
+# Imports
+# -------------------------------------------------------------------
 from vex import *
-import urandom
 
-brain = Brain()
+# Robot Configuration
+# -------------------------------------------------------------------
+brain          = Brain()
 brain_inertial = Inertial()
-paddle_2 = Motor(Ports.PORT1)
-paddle_1 = Motor(Ports.PORT2) 
-belt = Motor(Ports.PORT3, True)
-ai_vision = AiVision(Ports.PORT4, AiVision.ALL_TAGS)
-pusher = Motor(Ports.PORT5)
+paddle_2       = Motor(Ports.PORT1)
+paddle_1       = Motor(Ports.PORT2) 
+belt           = Motor(Ports.PORT3, True)
+ai_vision      = AiVision(Ports.PORT4, AiVision.ALL_TAGS)
+pusher         = Motor(Ports.PORT5)
 pusher.set_velocity(30, PERCENT)
+wait(100, MSEC) # ensure init
 
-wait(100, MSEC)
-
+# Constants
+# -------------------------------------------------------------------
 items = [
     {"id": 1, "name": "Milk",           "price": 1.99, "temp": "fridge"},
     {"id": 5, "name": "Cheese",         "price": 3.59, "temp": "fridge"},
@@ -28,7 +39,15 @@ items = [
     {"id": 29, "name": "Checkout"},
 ]
 
+shop = []
+
+# Motor Functions
+# -------------------------------------------------------------------
 def use_paddle(inputMotor, length) :
+    # Controls a motor to simulate a paddle movement.
+    # Args:
+    #     inputMotor: The motor object to control.
+    #     length: The duration in milliseconds to hold the paddle at the extended position.
     motor = inputMotor
     motor.set_position(0, DEGREES)
     motor.spin_to_position(70, DEGREES)
@@ -38,12 +57,14 @@ def use_paddle(inputMotor, length) :
     motor.stop()
 
 def drive_belt() :
+    # Controls a motor to turn the drive belt.
     belt.set_velocity(30, PERCENT)
     while True:
         belt.spin(FORWARD)
         wait(100, MSEC)
 
 def push_item() :
+    # Controls a motor to move an item from the initial platform onto the drive belt.
     motor = pusher
     motor.set_position(0, DEGREES)
     motor.spin_to_position(-160, DEGREES)
@@ -52,7 +73,12 @@ def push_item() :
     wait(2000, MSEC)  
     motor.stop()
 
+# Helper Functions
+# -------------------------------------------------------------------
 def ai_cam() :
+    # Uses the ai camera to detect labels.
+    # Returns:
+    #     int | None.
     ai_objects = ai_vision.take_snapshot(AiVision.ALL_TAGS)
     length = str(len(ai_objects))
     if len(ai_objects) > 0:
@@ -62,12 +88,22 @@ def ai_cam() :
     return None
 
 def get_item_from_id(scan_id) :
+    # Checks the items array for matching item.
+    # Args:
+    #     scan_id: int
+    # Returns:
+    #     item{} | None.
     for item in items:
         if scan_id == item["id"]: 
             return item 
     return None
 
+# Printer Functions
+# -------------------------------------------------------------------
 def print_item(item) :
+    # Prints the item content to the screen.
+    # Args:
+    #    item: item{}
     brain.screen.clear_screen()  
     brain.screen.set_cursor(1, 1)
     brain.screen.print(item["id"])
@@ -80,6 +116,7 @@ def print_item(item) :
     brain.screen.next_row()
 
 def print_shop() :
+    # Prints total price of the shop array.
     total = 0
     for item in shop :
         total = total + item["price"]
@@ -87,13 +124,18 @@ def print_shop() :
     brain.screen.set_cursor(1, 1)
     brain.screen.print('total: ', total)
 
-shop = []
-belt_thread = Thread(drive_belt)
+
+# Main Run File
+# -------------------------------------------------------------------
+# Start the drive belt on thread
+belt_thread = Thread(drive_belt) 
 while True:
 
+    # Scan present barcodes
     scan_id = None
     scan_id = ai_cam()
 
+    # Checkout edge case
     if scan_id is 29 : 
         brain.play_sound(SoundType.TADA)
         belt.set_velocity(0, PERCENT) 
@@ -101,32 +143,40 @@ while True:
         wait(5000, MSEC) 
         break
 
+    # Present item edge case
     elif scan_id is not None:
 
         brain.play_note(1, 4, 200, 50)
         matched_item = get_item_from_id(scan_id)
         
+        # Valid item edge case
         if matched_item :
             
             print_item(matched_item)
             shop.append(matched_item)
-
             wait(1000, MSEC) 
+
+            # Move valid item onto the conveyor belt
             pusher_thread = Thread(push_item)
 
+            # Sort item dependent on temperature edge case
+            # Direct item to fridge
             if matched_item["temp"] == "fridge" :
                 paddle_thread = Thread(use_paddle(paddle_2, 3000))
                 paddle_thread.stop()
 
+            # Direct item to freezer
             elif matched_item["temp"] == "frozen" :
                 paddle_thread = Thread(use_paddle(paddle_1, 1800))
                 paddle_thread.stop()
 
+            # Direct item to dry store
             else :
                 wait(3000, MSEC)
 
             pusher_thread.stop()
         
+        # Error handle invalid barcode
         else :
             brain.screen.clear_screen()  
             brain.screen.set_cursor(1, 1)
